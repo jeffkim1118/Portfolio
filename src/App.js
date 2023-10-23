d179cfd408bec3a443b40cb3b5de9dd33e340895
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-10-23 19:57:59 -0400

    installed react router, tailwind css, and react icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Navbar from './components/navbar'
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,14 @@
 
 export default function Navbar() {
     return(
-        <div>
-
+        <div className="bg-yellow-10 w-screen content-right">
+            <ul className="navbar flex ">
+                <li><a></a></li>
+                <li><a className="nav-link m-6" href="#">Home</a></li>
+                <li><a className="nav-link m-6" href="#">About</a></li>
+                <li><a className="nav-link m-6" href="#">Portfolio</a></li>
+                <li><a className="nav-link m-6" href="#">Contact</a></li>
+            </ul>
         </div>
     )
 }
