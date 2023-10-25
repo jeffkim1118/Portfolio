54334bb3cb83f674e32becf2297e7b3621680cba
yoonsung <jeffkim1118@outlook.com>
2023-10-24 20:48:55 -0400

    minor update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import Navbar from './components/navbar'
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/home';
+import About from './components/about';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <Navbar/>
-      <Routes>
+      <Home />
+      <About/>
+      
+      <Footer/>
+      {/* <Routes>
         <Route exact path='/' element={<Home />}/>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,26 @@
-export default function Home(){
-    return(
-        <div>
-            Home
+export default function Home() {
+  return (
+    <div className="home-component">
+      {/* <div className="grid-line">
+        <div className="home-container">
+          <div className="hero bg-gradient-to-r from-indigo-500 to-indigo-300 to-violet-500 shadow-2xl shadow-lg shadow-blue-500/50 rounded-lg p-10">
+            <h1 className="text-yellow-200">Hero here</h1>
+          </div>
         </div>
-    )
+      </div> */}
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there!</h1>
+            <p className="py-6">
+              My name is Yoonsung "Jeffrey" Kim.<br/>
+              Check out my portfolio website!
+            </p>
+            
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
