d1d3c914f816bea243e7a43020f0f21fcd85322e
yoonsung <jeffkim1118@outlook.com>
2023-11-28 22:11:44 -0500

    Attempting to create project likes counter using brute force method. Currently thinking about using index number of projects array to differentiate each project to keep track of each likes on each project.

--- api/.venv/api.py
+++ api/.venv/api.py
@@ -13,6 +13,13 @@ total_visitors = {
     'counter':0
 }
 
+project_likes = {
+    'first': 0,
+    'second': 0,
+    'third': 0,
+    'fourth': 0
+}
+
 @app.route('/')
 def hello_world():
     return "<p>Hello, world</p>"
@@ -34,3 +41,19 @@ def visitors():
         # breakpoint()
         return total_visitors
 
+
+@app.route('/likes', methods=['GET','POST'])
+@cross_origin()
+def likes(projectIndexNum):
+    global project_likes
+    if request.method == "POST":
+        if projectIndexNum == '1':
+            project_likes['first']+=1
+        elif projectIndexNum == '2':
+            project_likes['second']+=1
+        elif projectIndexNum == '3':
+            project_likes['third']+=1
+        elif projectIndexNum == '4':
+            project_likes['fourth']+=1
+    if request.method == "GET":
+        return project_likes
